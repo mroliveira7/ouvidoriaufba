01dd76c07ee44da327d245555dd255e66015a5a8
Ricardo <ricardo.oliveira545@gmail.com>
2014-07-15 13:51:12 -0300

    Adicionando novas funcionalidades

--- models/db_wizard.py
+++ models/db_wizard.py
@@ -11,8 +11,17 @@ db.define_table('t_db_sigeo',
           label=T('Local')),
     Field('f_descricao', type='string',
     	  label=T('Descrição')),
+    Field('f_foto', type='upload',
+          label=T('Foto')),
     auth.signature,
     format='%(f_assunto)s',
     migrate=settings.migrate)
+
+db.t_db_sigeo.f_tipo.requires=IS_IN_SET(('Patrimônio danificado','Outro1','Outro2'),error_message='Insira um valor válido')
+db.t_db_sigeo.f_foto.requires=IS_EMPTY_OR(IS_IMAGE(extensions=('jpeg', 'png', '.gif'),
+error_message=error_m['imagem']))
+db.t_db_sigeo.f_assunto.requires=IS_NOT_EMPTY(error_message=error_m['vazio'])
+db.t_db_sigeo.f_local.requires=IS_NOT_EMPTY(error_message=error_m['vazio'])
+db.t_db_sigeo.f_descricao.requires=IS_NOT_EMPTY(error_message=error_m['vazio'])
 
 db.define_table('t_db_sigeo_archive',db.t_db_sigeo,Field('current_record','reference t_db_sigeo',readable=False,writable=False))
--- models/menu.py
+++ models/menu.py
@@ -6,4 +6,4 @@ response.meta.description = settings.description
 response.menu = [
 (T('Index'),URL('default','index')==URL(),URL('default','index'),[]),
 (T('Consultar ou Enviar Irregularidade'),URL('default','db_sigeo_manage')==URL(),URL('default','db_sigeo_manage'),[]),
-]
+(T('Contate-nos'),URL('default','contact')==URL(),URL('default','contact'),[])]
